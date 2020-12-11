73090a5e73f3922742d1ee778ef11e98e8742bd3
chyppoli <claude.hyppolite@gmail.com>
2020-12-11 15:53:31 -0500

    renamed playerController file to trackController and update. add insert statements to seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+use track_db;
+ 
+insert into tracks (name,address,city,state,zipCode,trackType)
+values ('test track 1','1313 Mockingird Lane','Marietta','GA',30060,'standard'),
+ ('test track 2','1111 Main Peachtree Blvd','Kennesaw','GA','30144','standard'),
+ ('test track 3','5555 Atlernate Peachtree Blvd','Kennesaw','GA','30144','standard');
+
