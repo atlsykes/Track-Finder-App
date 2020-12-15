60fc7991110a63146e1a94407acdd2b23815d65e
Adebambo Adeshiyan <badeshiyan@gmail.com>
2020-12-14 19:22:54 -0500

    added append code to singletrack.js

--- public/assets/js/singleTrack.js
+++ public/assets/js/singleTrack.js
@@ -1,7 +1,16 @@
 $(document).ready(function () {
+  console.log("Hello world");
   let city = "Miami";
   let state = "Florida";
   let map = `<iframe class="text-center animated fadeIn delay-2s" frameborder="0" style="border:0;" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyClFZgU_nLZ7QpoqQC_IvIizDwaNpYsYsU&amp;q=${city}, ${state}" allowfullscreen="">
     </iframe>`;
   theLocationOnThePage.append(map);
+  const email = $("#email").val();
+  const password = $("#password").val();
+  const firstName = $("#firstName").val();
+  const lastName = $("#lastName").val();
+  console.log(email);
+  console.log(password);
+  console.log(firstName);
+  console.log(lastName);
 });
