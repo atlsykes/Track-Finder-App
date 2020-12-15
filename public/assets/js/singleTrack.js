4d29a810e1351faf5fa9a28026b4864ec1a56bb8
Adebambo Adeshiyan <badeshiyan@gmail.com>
2020-12-14 20:56:05 -0500

    updated jquery in singletrack.js

--- public/assets/js/singleTrack.js
+++ public/assets/js/singleTrack.js
@@ -1,16 +1,32 @@
 $(document).ready(function () {
-  console.log("Hello world");
-  let city = "Miami";
-  let state = "Florida";
-  let map = `<iframe class="text-center animated fadeIn delay-2s" frameborder="0" style="border:0;" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyClFZgU_nLZ7QpoqQC_IvIizDwaNpYsYsU&amp;q=${city}, ${state}" allowfullscreen="">
-    </iframe>`;
-  theLocationOnThePage.append(map);
-  const email = $("#email").val();
-  const password = $("#password").val();
-  const firstName = $("#firstName").val();
-  const lastName = $("#lastName").val();
-  console.log(email);
-  console.log(password);
-  console.log(firstName);
-  console.log(lastName);
+  $("#view-track").on("submit", function (e) {
+    e.preventDefault();
+    const trackName = $("#name").val();
+    const trackType = $("#trackType").val();
+    const address = $("#address").val();
+    const state = $("#state").val();
+    const zipCode = $("#zipCode").val();
+    const availability = $("#availability").val();
+    console.log(trackName);
+    console.log(trackType);
+    console.log(address);
+    console.log(state);
+    console.log(zipCode);
+    console.log(availability);
+    $.ajax({
+      method: "GET",
+      url: "/api/tracks/:id",
+      data: {
+        name,
+        trackType,
+        address,
+        availability,
+        state,
+        zipCode,
+        availability,
+      },
+    }).then((response) => {
+      window.location.replace("/tracks");
+    });
+  });
 });
