ce39d81e651ccc93b70b0f6914ca8b2d0fd01e8d
Beth Presten <bethpresten@gmail.com>
2020-12-15 17:06:34 -0500

    working on the javascript for adding the new track

--- public/assets/js/new-track.js
+++ public/assets/js/new-track.js
@@ -5,4 +5,33 @@ $(document).ready(function () {
             $(".popup-overlay, .popup-content").removeClass("active");
         });
     });
+    // name,address,city,state,zipCode,trackType,details (from seeds)
+    $("#add-track").click(function (e) {
+        e.preventDefault();
+        const name = $("#name").val();
+        const address = $("#address").val();
+        const city = $("#city").val();
+        const state = $("#state").val();
+        // const zipCode = $("#zipCode").val();
+        const trackType = $("#trackType").val();
+        // const availability = $("#availability").val();
+        const details = $("#details").val();
+
+        $.ajax({
+            method: "POST",
+            url: "/api/tracks",
+            data: {
+                name,
+                address,
+                city,
+                state,
+                // zipCode,
+                trackType,
+                // availability,
+                details,
+            },
+        }).then((response) => {
+            window.location.replace("/track/new");
+        });
+    });
 });
