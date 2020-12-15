bfe77fc1e01d175fa0a413a96906c05885522899
Adebambo Adeshiyan <badeshiyan@gmail.com>
2020-12-14 21:07:41 -0500

    updated singletrack.js file with map code

--- public/assets/js/singleTrack.js
+++ public/assets/js/singleTrack.js
@@ -4,10 +4,15 @@ $(document).ready(function () {
     const trackName = $("#name").val();
     const trackType = $("#trackType").val();
     const address = $("#address").val();
-    const city = $("#city").val();
-    const state = $("#state").val();
+    // const city = $("#city").val();
+    // const state = $("#state").val();
     const zipCode = $("#zipCode").val();
     const availability = $("#availability").val();
+    let city = "Miami";
+    let state = "Florida";
+    let map = `<iframe class="text-center animated fadeIn delay-2s" frameborder="0" style="border:0;" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyClFZgU_nLZ7QpoqQC_IvIizDwaNpYsYsU&amp;q=${city}, ${state}" allowfullscreen="">
+    </iframe>`;
+    theLocationOnThePage.append(map);
     console.log(trackName);
     console.log(trackType);
     console.log(address);
@@ -22,9 +27,11 @@ $(document).ready(function () {
         trackType,
         address,
         availability,
+        city,
         state,
         zipCode,
         availability,
+        map,
       },
     }).then((response) => {
       window.location.replace("/tracks");
