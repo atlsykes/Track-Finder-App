6ecd414aabb1359a014cd6da241686e7fae2ee98
Beth Presten <bethpresten@gmail.com>
2020-12-16 23:11:54 -0500

    update track is now taking user to all tracks page to view their recent update

--- public/assets/js/updateTrack.js
+++ public/assets/js/updateTrack.js
@@ -1,13 +1,15 @@
 $(document).ready(function () {
+  const city = $("#city-update").val();
+  console.log("city", city);
   $("#save-track-button").on("click", function (e) {
     e.preventDefault();
     console.log("you clicked this button!!!!!!!!!")
     const id = $("#track-id").val();
     const trackType = $("#track-type-update").val();
-    const city = $("#city-update");
-
+    const city = $("#city-update").val();
     const details = $("#details").val();
     console.log("id", id);
+    console.log("city", city);
 
     $.ajax({
       method: "PUT",
@@ -18,7 +20,7 @@ $(document).ready(function () {
       },
     }).then((response) => {
       console.log(response);
-      window.location.replace(`/tracks/${city}/view`);
+      window.location.replace("/tracks/" + city + "/view");
     }).fail(err => {
       alert("Error!");
     })
